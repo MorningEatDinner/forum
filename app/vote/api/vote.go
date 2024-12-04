647c571bdaa59b0b313b2aef4ea938f82a19b046
zhengxiaorui1998 <zhengxiaorui1998@gmail.com>
2024-12-04 21:44:14 +0800

    触发vote镜像构建

--- app/vote/api/vote.go
+++ app/vote/api/vote.go
@@ -27,6 +27,5 @@ func main() {
 	handler.RegisterHandlers(server, ctx)
 
 	fmt.Printf("Starting server at %s:%d...\n", c.Host, c.Port)
-	fmt.Printf("Starting server at %s:%d...\n", c.Host, c.Port)
 	server.Start()
 }
