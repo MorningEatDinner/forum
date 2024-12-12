a32c51f8948f7d30cabcd9cbbffd5c585b17ecee
zhengxiaorui1998 <zhengxiaorui1998@gmail.com>
2024-12-12 14:10:54 +0800

    增加日志

--- app/user/api/user.go
+++ app/user/api/user.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 
@@ -9,6 +10,7 @@ import (
 	"forum/app/user/api/internal/svc"
 
 	"github.com/zeromicro/go-zero/core/conf"
+	"github.com/zeromicro/go-zero/core/logx"
 	"github.com/zeromicro/go-zero/rest"
 )
 
@@ -19,14 +21,22 @@ func main() {
 
 	var c config.Config
 	conf.MustLoad(*configFile, &c)
+	// 配置日志信息
+	logx.MustSetup(logx.LogConf{
+		ServiceName: "user-api",
+		Mode:        "file",
+		Path:        "/var/log/user-api", // 改为与 volumeMount 中配置的 mountPath 一致
+		Compress:    true,                // 是否压缩
+		KeepDays:    7,                   // 日志保留天数
+	})
 
 	server := rest.MustNewServer(c.RestConf)
 	defer server.Stop()
 
 	ctx := svc.NewServiceContext(c)
 	handler.RegisterHandlers(server, ctx)
-	fmt.Println("Starting server at %s:%d...", c.Host, c.Port)
-
 	fmt.Printf("Starting server at %s:%d...\n", c.Host, c.Port)
+	logx.WithContext(context.Background()).Info("容器启动成功")
+
 	server.Start()
 }
